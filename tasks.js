e458dff9c0f5bb43e491a151c6ab16f229717a89
Pavithr Jain <pavithrjain19@gmail.com>
2025-05-18 17:07:18 +0530

    first commit

---
+++ tasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+    {
+        id: 1,
+        title: "Node.js",
+        status: "completed"
+    },
+    {
+        id: 2,
+        title: "Express.js",
+        status: "pending"
+    }
+];
+
+module.exports = tasks;
